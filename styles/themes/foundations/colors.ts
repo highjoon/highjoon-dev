126a65ff35b4165f5174b4785cb12acda72964bc
highjoon <yundosa2@gmail.com>
2023-07-18 01:32:18 +0900

    style: foundation 색상 추가 설정

--- styles/themes/foundations/colors.ts
+++ styles/themes/foundations/colors.ts
@@ -1,5 +1,15 @@
 export const colors = {
-	black: "var(--color-black)",
-	white: "var(--color-white)",
-	primary: "var(--color-primary)",
+  black: 'var(--color-black)',
+  white: 'var(--color-white)',
+  primary: {
+    50: 'var(--color-primary-50)',
+    500: 'var(--color-primary-500)',
+  },
+  grey: {
+    200: 'var(--color-grey-200)',
+    900: 'var(--color-grey-900)',
+  },
+  secondary: {
+    100: 'var(--color-secondary-100)',
+  },
 } as const;
