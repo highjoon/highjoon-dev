ac53f050523c4c9330c21458e58da2a04828db85
highjoon <yundosa2@gmail.com>
2023-08-03 02:24:57 +0900

    refactor: gray-matter data type assertion

--- services/utils/getBlogPosts.ts
+++ services/utils/getBlogPosts.ts
@@ -3,13 +3,19 @@ import matter from 'gray-matter';
 import path from 'path';
 import { BLOG_CONTENTS_DIR } from '../constants/blogs';
 
+type FrontMatter = {
+  title: string;
+  description: string;
+  date: string;
+};
+
 const getBlogPosts = (files: string[]) => {
   return files.map((fileName) => {
     const fileContent = readFileSync(path.join(BLOG_CONTENTS_DIR, fileName), 'utf-8');
     const { data: frontMatter, content } = matter(fileContent);
 
     return {
-      meta: frontMatter,
+      meta: frontMatter as FrontMatter,
       slug: fileName.replace('.mdx', ''),
       content,
     };
