0418914cb19ef647864c736f64000a251690681f
highjoon <yundosa2@gmail.com>
2023-08-07 01:03:58 +0900

    style: TOC 디자인 수정

--- components/Post/TableOfContents.tsx
+++ components/Post/TableOfContents.tsx
@@ -34,19 +34,28 @@ const TableOfContents = () => {
   }, []);
 
   return (
-    <ul className="flex flex-col border-l-[1px] fixed" style={{ margin: 0, paddingLeft: '10px' }}>
-      {headingElements.map((element, index) => (
-        <li key={String(element) + index} className="list-none" style={{ margin: 0, paddingLeft: 0 }}>
-          <Link
-            href={`#${element.id}`}
-            onClick={handleClickLink}
-            className={`${
-              element.id === activeId ? 'text-primary-500' : 'text-grey-900'
-            } py-1 w-fit cursor-pointer transition-all duration-75 no-underline`}>
-            {element.innerHTML}
-          </Link>
-        </li>
-      ))}
+    <ul
+      className="fixed flex flex-col overflow-y-scroll border-l-2 border-grey-600"
+      style={{
+        margin: 0,
+        paddingLeft: '15px',
+        paddingRight: '15px',
+        maxHeight: 'calc(100vh - 128px)',
+      }}>
+      {headingElements.length > 0
+        ? headingElements.map((element, index) => (
+            <li key={String(element) + index} className="py-1 text-sm list-none" style={{ margin: 0, paddingLeft: 0 }}>
+              <Link
+                href={`#${element.id}`}
+                onClick={handleClickLink}
+                className={`${
+                  element.id === activeId ? 'text-primary-500' : 'text-grey-600'
+                } py-1 w-fit cursor-pointer transition-all duration-75 no-underline`}>
+                {element.innerHTML}
+              </Link>
+            </li>
+          ))
+        : null}
     </ul>
   );
 };
