58600e547c9baf0a49228f7c0a3193950f0d4485
highjoon <yundosa2@gmail.com>
2023-08-13 21:28:14 +0900

    feat: Mock post data 추가

--- services/constants/blogPosts.ts
+++ services/constants/blogPosts.ts
@@ -4,8 +4,13 @@ export const POSTS_FILE_NAME = {
   FRONTEND_INTERVIEW_QUESTIONS: 'frontend-interview-questions',
   REACT_QUERY_WITH_NEXTJS_SERVER_SIDE_RENDERING: 'react-query-with-nextjs-server-side-rendering',
   COMPLETE_GUIDE_TO_REACT_RENDERING_BEHAVIOR: 'complete-guide-to-react-rendering-behavior',
+  PROPER_USAGE_OF_USE_SELECTOR: 'proper-usage-of-use-selector',
 } as const;
 
 export type POSTS_FILE_NAME = typeof POSTS_FILE_NAME;
 
 export const GNB_HEIGHT = 92;
+
+export const POSTS_PER_PAGE = 5;
+
+export const DEFAULT_NUMBER_OF_POSTS_PER_PAGE = 5;
--- services/data/posts.ts
+++ services/data/posts.ts
@@ -4,6 +4,41 @@ import createBannerImgPath from '../utils/createBannerImgPath';
 
 export const posts: Post[] = [
   {
+    fileName: POSTS_FILE_NAME.PROPER_USAGE_OF_USE_SELECTOR,
+    title: 'useSelector의 적절한 사용법',
+    description: 'React-Redux useSelector의 적절한 사용법',
+    date: '2023-03-05',
+    bannerImg: createBannerImgPath(POSTS_FILE_NAME.PROPER_USAGE_OF_USE_SELECTOR),
+  },
+  {
+    fileName: POSTS_FILE_NAME.PROPER_USAGE_OF_USE_SELECTOR,
+    title: 'useSelector의 적절한 사용법',
+    description: 'React-Redux useSelector의 적절한 사용법',
+    date: '2023-03-05',
+    bannerImg: createBannerImgPath(POSTS_FILE_NAME.PROPER_USAGE_OF_USE_SELECTOR),
+  },
+  {
+    fileName: POSTS_FILE_NAME.PROPER_USAGE_OF_USE_SELECTOR,
+    title: 'useSelector의 적절한 사용법',
+    description: 'React-Redux useSelector의 적절한 사용법',
+    date: '2023-03-05',
+    bannerImg: createBannerImgPath(POSTS_FILE_NAME.PROPER_USAGE_OF_USE_SELECTOR),
+  },
+  {
+    fileName: POSTS_FILE_NAME.PROPER_USAGE_OF_USE_SELECTOR,
+    title: 'useSelector의 적절한 사용법',
+    description: 'React-Redux useSelector의 적절한 사용법',
+    date: '2023-03-05',
+    bannerImg: createBannerImgPath(POSTS_FILE_NAME.PROPER_USAGE_OF_USE_SELECTOR),
+  },
+  {
+    fileName: POSTS_FILE_NAME.PROPER_USAGE_OF_USE_SELECTOR,
+    title: 'useSelector의 적절한 사용법',
+    description: 'React-Redux useSelector의 적절한 사용법',
+    date: '2023-03-05',
+    bannerImg: createBannerImgPath(POSTS_FILE_NAME.PROPER_USAGE_OF_USE_SELECTOR),
+  },
+  {
     fileName: POSTS_FILE_NAME.FRONTEND_INTERVIEW_QUESTIONS,
     title: '지금까지 받았던 신입 프론트엔드 면접 질문들',
     description: '그동안 면접에서 받았던 질문들 정리',
