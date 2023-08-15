ea0b4d79415e5a0fd8b3db9ba2715dfd5bf97310
highjoon <yundosa2@gmail.com>
2023-08-15 22:59:54 +0900

    style: post 반응형

--- components/Post/PostCard.tsx
+++ components/Post/PostCard.tsx
@@ -12,9 +12,9 @@ const PostCard = ({ title, description, date, bannerImg }: Props) => {
     <div className="max-w-[768px] w-full max-h-[400px] md:max-h-[570px] mx-auto hover:bg-grey-50 cursor-pointer border-b-[1px] border-b-grey-100">
       <Image src={bannerImg} width={768} height={400} alt="post-logo" priority />
       <div className="flex flex-col justify-between gap-2 p-2">
-        <p className="text-xl font-bold text-grey-900">{title}</p>
-        <p className="text-grey-700">{description}</p>
-        <p className="text-grey-400">{date}</p>
+        <p className="text-lg font-bold lg:text-xl text-grey-900">{title}</p>
+        <p className="text-sm lg:text-lg text-grey-700">{description}</p>
+        <p className="text-sm lg:text-lg text-grey-400">{date}</p>
       </div>
     </div>
   );
--- components/Post/RecentPostsLayout.tsx
+++ components/Post/RecentPostsLayout.tsx
@@ -6,7 +6,7 @@ import { ROUTES } from '@/services/constants/routes';
 const RecentPostsLayout = ({ children }: PropsWithChildren) => {
   return (
     <div className="py-2 flex flex-col max-w-[768px] mx-auto gap-6">
-      <h1 className="text-5xl font-bold text-grey-900">Recent Posts</h1>
+      <h1 className="text-2xl font-bold sm:text-3xl md:text-4xl lg:text-5xl text-grey-900">Recent Posts</h1>
       {children}
       <div className="flex justify-end">
         <Link href={ROUTES.PAGES + '1'} className="text-grey-600 hover:text-primary-500">
