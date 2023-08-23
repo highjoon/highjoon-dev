998f9f011f85b6c529b39bb86d4f3527be877ae1
highjoon <yundosa2@gmail.com>
2023-08-24 01:10:30 +0900

    refactor: home 접근 시 포스트 페이지로 리다이렉트 처리

---
+++ middleware.ts
@@ -0,0 +1,11 @@
+import { NextRequest, NextResponse } from 'next/server';
+
+export function middleware(request: NextRequest) {
+  if (request.nextUrl.pathname === '/') {
+    return NextResponse.redirect(new URL('/pages/1', request.nextUrl));
+  }
+}
+
+export const config = {
+  matcher: '/',
+};
