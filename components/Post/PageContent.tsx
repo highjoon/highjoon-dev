1f60ba159abc9437e8698f0e323fd02f36823ee0
highjoon <yundosa2@gmail.com>
2023-08-29 01:50:27 +0900

    refactor: PageContent Image style tailwind 적용

--- components/Post/PageContent.tsx
+++ components/Post/PageContent.tsx
@@ -12,14 +12,7 @@ const PageContent = ({ title, bannerImg, content }: Props) => {
   return (
     <article className="relative mx-auto prose-sm prose md:prose-base lg:prose-lg prose-slate mb-[200px]">
       <h1>{title}</h1>
-      <Image
-        src={bannerImg}
-        width={768}
-        height={400}
-        alt="content-logo"
-        priority
-        style={{ marginTop: '0px', marginBottom: '0px' }}
-      />
+      <Image className="my-0" src={bannerImg} width={768} height={400} alt="content-logo" priority />
       <div className="absolute top-0 right-[-50px] hidden xl:block">
         <TableOfContents />
       </div>
