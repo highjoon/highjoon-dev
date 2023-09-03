cf978bd3f7f5c5e5e07d4bf4604f03df841da2ae
highjoon <yundosa2@gmail.com>
2023-09-03 23:26:34 +0900

    refactor: Tab Bar 컴포넌트 분리 및 상수화

--- components/Common/TabBar.tsx
+++ components/Common/TabBar.tsx
@@ -4,6 +4,21 @@ import Link from 'next/link';
 import { usePathname } from 'next/navigation';
 
 import { ROUTES } from '@/services/constants/routes';
+import { TAB_NAME } from '@/services/constants/tab';
+
+const Tab = ({ href, tabName, isActive }: { href: string; tabName: string; isActive: boolean }) => {
+  return (
+    <Link
+      href={href}
+      className={`flex-1 py-2 text-center border-t-2 ${
+        isActive
+          ? 'border-t-primary-500 text-primary-500'
+          : 'border-t-grey-100 text-grey-400 dark:text-white dark:border-t-white'
+      }`}>
+      <li className="text-xl font-bold">{tabName}</li>
+    </Link>
+  );
+};
 
 const TabBar = () => {
   const pathname = usePathname();
@@ -12,33 +27,21 @@ const TabBar = () => {
 
   return (
     <ul className="flex items-center justify-between w-full">
-      <Link
-        href={ROUTES.HOME}
-        className={`flex-1 py-2 text-center border-t-2 ${
-          pathname.includes(ROUTES.PAGES) || ROUTES.PAGES.includes(pathname)
-            ? 'border-t-primary-500 text-primary-500'
-            : 'border-t-grey-100 text-grey-400 dark:text-white dark:border-t-white'
-        }`}>
-        <li className="text-xl font-bold">Posts</li>
-      </Link>
-      <Link
+      <Tab
+        href={ROUTES.PAGES + '1'}
+        tabName={TAB_NAME.POSTS}
+        isActive={pathname !== ROUTES.HOME && (pathname.includes(ROUTES.PAGES) || ROUTES.PAGES.includes(pathname))}
+      />
+      <Tab
         href={ROUTES.TAGS}
-        className={`flex-1 py-2 text-center border-t-2 ${
-          pathname.includes(ROUTES.TAGS) || ROUTES.TAGS.includes(pathname)
-            ? 'border-t-primary-500 text-primary-500'
-            : 'border-t-grey-100 text-grey-400 dark:text-white dark:border-t-white'
-        }`}>
-        <li className="text-xl font-bold">Tags</li>
-      </Link>
-      <Link
+        tabName={TAB_NAME.TAGS}
+        isActive={pathname !== ROUTES.HOME && (pathname.includes(ROUTES.TAGS) || ROUTES.TAGS.includes(pathname))}
+      />
+      <Tab
         href={ROUTES.ABOUT}
-        className={`flex-1 py-2 text-center border-t-2 ${
-          pathname.includes(ROUTES.ABOUT) || ROUTES.ABOUT.includes(pathname)
-            ? 'border-t-primary-500 text-primary-500'
-            : 'border-t-grey-100 text-grey-400 dark:text-white dark:border-t-white'
-        }`}>
-        <li className="text-xl font-bold">About</li>
-      </Link>
+        tabName={TAB_NAME.ABOUT}
+        isActive={pathname !== ROUTES.HOME && (pathname.includes(ROUTES.ABOUT) || ROUTES.ABOUT.includes(pathname))}
+      />
     </ul>
   );
 };
---
+++ services/constants/tab.ts
@@ -0,0 +1,5 @@
+export const TAB_NAME = {
+  POSTS: 'Posts',
+  TAGS: 'Tags',
+  ABOUT: 'About',
+} as const;
