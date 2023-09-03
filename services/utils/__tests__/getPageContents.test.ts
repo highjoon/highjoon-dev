1b0253da58b2b6ccb8e921e82ea8fdbcd80dad4d
highjoon <yundosa2@gmail.com>
2023-09-03 23:54:55 +0900

    test: getPageContens 수정

--- services/utils/__tests__/getPageContents.test.ts
+++ services/utils/__tests__/getPageContents.test.ts
@@ -6,14 +6,14 @@ import { posts } from '@/services/data/posts';
 describe('getPageContents', () => {
   it('첫 페이지일 경우 올바른 페이지 컨텐츠를 반환해야 합니다.', () => {
     const pageNumber = 1;
-    const { currentPagePosts } = getPageContents(pageNumber);
+    const { currentPagePosts } = getPageContents(posts, pageNumber);
 
     expect(currentPagePosts).toEqual(posts.slice(0, DEFAULT_NUMBER_OF_POSTS_PER_PAGE));
   });
 
   it('중간 페이지일 경우 올바른 페이지 컨텐츠를 반환해야 합니다.', () => {
     const pageNumber = 2;
-    const { currentPagePosts } = getPageContents(pageNumber);
+    const { currentPagePosts } = getPageContents(posts, pageNumber);
 
     const startIndex = (pageNumber - 1) * DEFAULT_NUMBER_OF_POSTS_PER_PAGE;
     const endIndex = startIndex + 5;
@@ -25,7 +25,7 @@ describe('getPageContents', () => {
 
   it('마지막 페이지일 경우 올바른 페이지 컨텐츠를 반환해야 합니다.', () => {
     const pageNumber = Math.ceil(posts.length / DEFAULT_NUMBER_OF_POSTS_PER_PAGE);
-    const { currentPagePosts } = getPageContents(pageNumber);
+    const { currentPagePosts } = getPageContents(posts, pageNumber);
 
     const startIndex = (pageNumber - 1) * DEFAULT_NUMBER_OF_POSTS_PER_PAGE;
     const expectedPosts = posts.slice(startIndex);
