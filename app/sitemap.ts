98900e38d3afbac364f69e1e473cac3ed4036550
윤상준 <yundosa2@mobidoo.co.kr>
2024-01-04 01:02:09 +0900

    feat: robot, sitemap

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -1,10 +1,28 @@
 import { MetadataRoute } from 'next';
 
+import { posts } from '@/data/posts';
+import getAllTagsFromPosts from '@/utils/getAllTagsFromPosts';
+
 export default function sitemap(): MetadataRoute.Sitemap {
   return [
     {
       url: 'https://highjoon-dev.vercel.app',
       lastModified: new Date(),
     },
+    {
+      url: 'https://highjoon-dev.vercel.app/about',
+      lastModified: new Date(),
+    },
+    ...posts.map((post) => {
+      return {
+        url: `https://highjoon-dev.vercel.app/blogs/${post.fileName}`,
+        lastModified: new Date(post.date),
+      };
+    }),
+    ...getAllTagsFromPosts().map((tag) => {
+      return {
+        url: `https://highjoon-dev.vercel.app/tags/${tag}`,
+      };
+    }),
   ];
 }
