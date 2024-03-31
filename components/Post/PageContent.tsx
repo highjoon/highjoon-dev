672fbf44d278a39d1c704e47ae45dfacd519612a
highjoon <yundosa2@gmail.com>
2024-03-31 20:56:39 +0900

    feat: giscus comments

--- components/Post/PageContent.tsx
+++ components/Post/PageContent.tsx
@@ -1,5 +1,6 @@
 import Image from 'next/image';
 import MDXContent from '../MDX/MDXContent';
+import Comments from './Comments';
 import TableOfContents from './TableOfContents';
 
 type Props = {
@@ -10,14 +11,17 @@ type Props = {
 
 const PageContent = ({ title, bannerImg, content }: Props) => {
   return (
-    <article className="w-full relative mx-auto prose-sm prose md:prose-base lg:prose-lg prose-slate mb-[200px] dark:prose-invert">
-      <h1>{title}</h1>
-      <Image className="my-0" src={bannerImg} width={768} height={400} alt="content-logo" priority />
-      <div className="absolute top-0 right-[-50px] hidden xl:block">
-        <TableOfContents />
-      </div>
-      <MDXContent source={content} />
-    </article>
+    <>
+      <article className="w-full relative mx-auto prose-sm prose md:prose-base lg:prose-lg prose-slate mb-[20px] dark:prose-invert">
+        <h1>{title}</h1>
+        <Image className="my-0" src={bannerImg} width={768} height={400} alt="content-logo" priority />
+        <div className="absolute top-0 right-[-50px] hidden xl:block">
+          <TableOfContents />
+        </div>
+        <MDXContent source={content} />
+      </article>
+      <Comments />
+    </>
   );
 };
 
