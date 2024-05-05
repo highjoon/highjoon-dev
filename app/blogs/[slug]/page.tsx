30d7099fcbe080cd8449595aac5a6812ac25af05
highjoon <yundosa2@gmail.com>
2024-05-06 01:42:25 +0900

    refactor: 블로그 포스트 페이지 컴포넌트 분리

--- app/blogs/[slug]/page.tsx
+++ app/blogs/[slug]/page.tsx
@@ -1,14 +1,10 @@
 import { Metadata } from 'next';
-import Image from 'next/image';
-import { Box, Flex, Title } from '@mantine/core';
 import fs from 'fs';
 import path from 'path';
 import Comments from '@/components/comments/Comments';
-import { MdxComponents } from '@/components/mdxComponents/MdxComponents';
-import MDXContent from '@/components/mdxContent/MDXContent';
+import PageContent from '@/components/pageContent/PageContent';
 import { BLOG_CONTENTS_DIR } from '@/constants/blogPosts';
 import getBlogPost from '@/utils/getBlogPost';
-import styles from './page.module.scss';
 
 export async function generateStaticParams() {
   const files = fs.readdirSync(path.join(BLOG_CONTENTS_DIR));
@@ -34,17 +30,7 @@ export default function Page({ params }: any) {
 
   return (
     <>
-      <Flex direction="column" w="100%" pos="relative" mb={20}>
-        <Title order={1} className={styles.title}>
-          {frontMatter.title}
-        </Title>
-        <Box className={styles.banner} pos="relative" w="100%">
-          <Image src={frontMatter.bannerImg} fill alt="content-logo" priority />
-        </Box>
-        <Flex id="page-content" direction="column">
-          <MDXContent source={content} components={MdxComponents} />
-        </Flex>
-      </Flex>
+      <PageContent frontMatter={frontMatter} content={content} />
       <Comments />
     </>
   );
