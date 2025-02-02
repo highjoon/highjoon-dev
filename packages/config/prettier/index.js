44d49687797103e4ff33c563f4e9df1924bfdbb9
highjoon <yundosa2@gmail.com>
2025-02-02 18:08:45 +0900

    chore: eslint, prettier config 패키지 분리

---
+++ packages/config/prettier/index.js
@@ -0,0 +1,10 @@
+/** @type {import('prettier').Config} **/
+module.exports = {
+  singleQuote: true,
+  bracketSpacing: true,
+  bracketSameLine: true,
+  useTabs: false,
+  tabWidth: 2,
+  printWidth: 120,
+  arrowParens: "always",
+};
