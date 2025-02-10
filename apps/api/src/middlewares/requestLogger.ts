fee59333a804099661978581e8bd5ebba80068a9
highjoon <yundosa2@gmail.com>
2025-02-10 22:57:54 +0900

    chore: requestLogger

---
+++ apps/api/src/middlewares/requestLogger.ts
@@ -0,0 +1,95 @@
+import type { Request, RequestHandler, Response } from 'express';
+import { getReasonPhrase, StatusCodes } from 'http-status-codes';
+import { randomUUID } from 'node:crypto';
+import type { IncomingMessage, ServerResponse } from 'node:http';
+import type { LevelWithSilent } from 'pino';
+import { type CustomAttributeKeys, type Options, pinoHttp } from 'pino-http';
+
+import { type LoggerRequestProps, LogLevel } from '@/types/logger';
+import { env } from '@/utils/env';
+
+const customAttributeKeys: CustomAttributeKeys = {
+  req: 'request',
+  res: 'response',
+  err: 'error',
+  responseTime: 'timeTaken',
+};
+
+const customProps = (req: Request, res: Response): LoggerRequestProps => ({
+  request: req,
+  response: res,
+  error: res.locals.err,
+  responseBody: res.locals.responseBody,
+});
+
+const responseBodyMiddleware: RequestHandler = (_req, res, next) => {
+  const isNotProduction = !env.isProduction;
+
+  if (isNotProduction) {
+    const originalSend = res.send;
+
+    res.send = (content) => {
+      res.locals.responseBody = content;
+      res.send = originalSend;
+
+      return originalSend.call(res, content);
+    };
+  }
+  next();
+};
+
+const customLogLevel = (_req: IncomingMessage, res: ServerResponse<IncomingMessage>, err?: Error): LevelWithSilent => {
+  if (err || res.statusCode >= StatusCodes.INTERNAL_SERVER_ERROR) {
+    return LogLevel.Error;
+  }
+
+  if (res.statusCode >= StatusCodes.BAD_REQUEST) {
+    return LogLevel.Warn;
+  }
+
+  if (res.statusCode >= StatusCodes.MULTIPLE_CHOICES) {
+    return LogLevel.Silent;
+  }
+
+  return LogLevel.Info;
+};
+
+const customSuccessMessage = (req: IncomingMessage, res: ServerResponse<IncomingMessage>) => {
+  if (res.statusCode === StatusCodes.NOT_FOUND) {
+    return getReasonPhrase(StatusCodes.NOT_FOUND);
+  }
+
+  return `${req.method} completed`;
+};
+
+const genReqId = (req: IncomingMessage, res: ServerResponse<IncomingMessage>) => {
+  const existingID = req.id ?? req.headers['x-request-id'];
+
+  if (existingID) {
+    return existingID;
+  }
+
+  const id = randomUUID();
+  res.setHeader('X-Request-Id', id);
+
+  return id;
+};
+
+const requestLogger = (options?: Options): RequestHandler[] => {
+  const pinoOptions: Options = {
+    enabled: env.isProduction,
+    customProps: customProps as unknown as Options['customProps'],
+    redact: [],
+    genReqId,
+    customLogLevel,
+    customSuccessMessage,
+    customReceivedMessage: (req) => `request received: ${req.method}`,
+    customErrorMessage: (_req, res) => `request errored with status code: ${res.statusCode}`,
+    customAttributeKeys,
+    ...options,
+  };
+
+  return [responseBodyMiddleware, pinoHttp(pinoOptions)];
+};
+
+export default requestLogger();
--- apps/api/src/server.ts
+++ apps/api/src/server.ts
@@ -3,6 +3,7 @@ import express from 'express';
 import helmet from 'helmet';
 import { pino } from 'pino';
 
+import requestLogger from '@/middlewares/requestLogger';
 import { env } from '@/utils/env';
 
 const logger = pino({ name: 'server start' });
@@ -15,6 +16,9 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cors({ origin: env.CORS_ORIGIN, credentials: true }));
 app.use(helmet());
+
+// Request logging
+app.use(requestLogger);
 
 // Routes
 app.get('/', (req, res) => {
