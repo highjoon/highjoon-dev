0743290465481f7c31033ccfdb67439a03be691a
highjoon <yundosa2@gmail.com>
2025-03-16 06:21:19 +0900

    refactor: 최근 게시물 api 로직 위치 변경

--- apps/web/src/actions/post.ts
+++ apps/web/src/actions/post.ts
@@ -3,9 +3,30 @@
 import { revalidatePath } from 'next/cache';
 import { type Post } from '@highjoon-dev/types';
 
-import { increaseViewCount } from '@/apis/post';
+import { getPostList, increaseViewCount } from '@/apis/post';
+import sortPostsByDate from '@/utils/sortPostsByDate';
 
 export const increaseViewCountAction = async (slug: Post['slug']) => {
   await increaseViewCount(slug);
   revalidatePath(`/post/${slug}`);
+};
+
+export const getRecentPosts = async () => {
+  const splitArray = (array: Post[]) => {
+    const result = [];
+
+    for (let index = 0; index < array.length; index += 3) {
+      let tempArray = [];
+      tempArray = array.slice(index, index + 3);
+      result.push(tempArray);
+    }
+
+    return result;
+  };
+
+  const postList = await getPostList();
+  const sortedPostsByDate = sortPostsByDate(postList.responseObject).slice(0, 9);
+  const splittedPosts = splitArray(sortedPostsByDate);
+
+  return splittedPosts;
 };
