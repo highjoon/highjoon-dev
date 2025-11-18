a9687a7af0345b53c15bbeec00cfc3f1d9981720
highjoon <yundosa2@gmail.com>
2025-11-19 00:08:10 +0900

    chore: remove google adsense script

--- apps/web/src/app/scripts/AppScripts.tsx
+++ apps/web/src/app/scripts/AppScripts.tsx
@@ -2,14 +2,12 @@ import React from 'react';
 import { Analytics } from '@vercel/analytics/react';
 import { SpeedInsights } from '@vercel/speed-insights/next';
 
-import GoogleAdsense from '@/app/scripts/GoogleAdsense';
 import GoogleAnalytics from '@/app/scripts/GoogleAnalytics';
 import WebsiteSchema from '@/app/seo/WebsiteSchema';
 
 export default function AppScripts() {
   return (
     <>
-      <GoogleAdsense />
       <WebsiteSchema />
       <GoogleAnalytics />
       <Analytics />
