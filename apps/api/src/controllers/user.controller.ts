b23f246b6f2890b0513684fb7f6ecadaf510e2c2
highjoon <yundosa2@gmail.com>
2025-11-23 00:13:14 +0900

    feat: user 도메인 개선

--- apps/api/src/controllers/user.controller.ts
+++ apps/api/src/controllers/user.controller.ts
@@ -1,22 +1,11 @@
 import { type Request, type Response } from 'express';
-import { StatusCodes } from 'http-status-codes';
 
-import { ServiceResponse } from '@/models/servicesResponse';
 import { userService } from '@/services/user/user.service';
 import { handleServiceResponse } from '@/utils/httpHandlers';
 
 class UserController {
   public getLikedPostsByUserId = async (req: Request, res: Response) => {
     const userId = req.params.userId;
-
-    if (!userId) {
-      handleServiceResponse(
-        ServiceResponse.failure('유효하지 않은 사용자 ID입니다.', null, StatusCodes.BAD_REQUEST),
-        res,
-      );
-
-      return;
-    }
 
     const postsResponse = await userService.findLikedPostsByUserId(userId);
 
--- apps/api/src/routes/user.routes.ts
+++ apps/api/src/routes/user.routes.ts
@@ -2,7 +2,14 @@ import express, { type Router } from 'express';
 
 import { userController } from '@/controllers/user.controller';
 import { authenticate } from '@/middlewares/authenticate';
+import { validateRequest } from '@/middlewares/validateRequest';
+import { getLikedPostsByUserIdParamsSchema } from '@/schemas/user.schema';
 
 export const userRoutes: Router = express.Router();
 
-userRoutes.get('/:userId/liked-posts', authenticate, userController.getLikedPostsByUserId);
+userRoutes.get(
+  '/:userId/liked-posts',
+  authenticate,
+  validateRequest({ params: getLikedPostsByUserIdParamsSchema }),
+  userController.getLikedPostsByUserId,
+);
