2354b81d8309091aa870937da023e886068434c2
highjoon <yundosa2@gmail.com>
2025-12-20 18:41:01 +0900

    feat: Tag 라우트 추가

---
+++ apps/api/src/routes/tag.routes.ts
@@ -0,0 +1,19 @@
+import express, { type Router } from 'express';
+
+import { tagController } from '@/controllers/tag.controller';
+import { authenticate } from '@/middlewares/authenticate';
+import { validateRequest } from '@/middlewares/validateRequest';
+import { createTagSchema, tagIdParamsSchema, updateTagSchema } from '@/schemas/tag.schema';
+
+export const tagRoutes: Router = express.Router();
+
+tagRoutes.get('/', tagController.getAllTags);
+tagRoutes.get('/:id', validateRequest({ params: tagIdParamsSchema }), tagController.getTag);
+tagRoutes.post('/', authenticate, validateRequest({ body: createTagSchema }), tagController.createTag);
+tagRoutes.put(
+  '/:id',
+  authenticate,
+  validateRequest({ params: tagIdParamsSchema, body: updateTagSchema }),
+  tagController.updateTag,
+);
+tagRoutes.delete('/:id', authenticate, validateRequest({ params: tagIdParamsSchema }), tagController.deleteTag);
