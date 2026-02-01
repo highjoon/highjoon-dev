036400dc2790234c14924259d3d7d6f4cd3e0ce5
highjoon <yundosa2@gmail.com>
2026-02-02 00:36:42 +0900

    refactor: renewal 좋아요, 댓글 영역

--- apps/web/src/widgets/likeCommentsSection/ui/LikeCommentsSection.tsx
+++ apps/web/src/widgets/likeCommentsSection/ui/LikeCommentsSection.tsx
@@ -1,7 +1,8 @@
 import React from 'react';
 import { Post } from '@highjoon-dev/prisma';
+import { MessageSquare } from 'lucide-react';
 
-import LikeButton from '@/features/likePost/ui/LikeButton';
+import InteractionBar from '@/entities/post/ui/postDetail/InteractionBar';
 import CommentSection from '@/widgets/commentSection/ui/CommentSection';
 
 type Props = {
@@ -10,9 +11,16 @@ type Props = {
 
 export default function LikeCommentsSection({ post }: Props) {
   return (
-    <div className="flex flex-col items-center justify-center gap-4 my-5">
-      <LikeButton postId={post.id} likeCount={post.likeCount} slug={post.slug} />
-      <CommentSection postId={post.id} />
+    <div className="flex flex-col">
+      <InteractionBar postId={post.id} likeCount={post.likeCount} slug={post.slug} />
+
+      <div className="mb-20">
+        <h3 className="flex items-center gap-3 mb-8 text-2xl font-black text-vibrant-text-main">
+          <MessageSquare className="text-vibrant-brand" />
+          Comments
+        </h3>
+        <CommentSection postId={post.id} />
+      </div>
     </div>
   );
 }
